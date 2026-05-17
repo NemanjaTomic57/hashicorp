4a1f1cc9c8c86c1c3f42daaaa7c6071d17577360
Nemanja Tomic <nemanja.tomic@ik.me>
2026-05-17 15:37:13 +0200

    update kafka-lab

--- vagrant/kafka-lab/workspace/main.go
+++ vagrant/kafka-lab/workspace/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -12,27 +11,26 @@ import (
 	"github.com/joho/godotenv"
 )
 
-type namespace struct {
-	id        int
-	name      string
-	path      string
-	kind      string
-	full_path string
-	web_url   string
+type Namespace struct {
+	Id       int    `json:"id"`
+	Name     string `json:"name"`
+	Path     string `json:"path"`
+	Kind     string `json:"kind"`
+	FullPath string `json:"full_path"`
+	WebURL   string `json:"web_url"`
 }
 
-type gitlabProject struct {
-	id                  int
-	description         string
-	path_with_namespace string
-	created_at          string
-	web_url             string
-	namespace           namespace
+type GitlabProject struct {
+	Id                int       `json:"id"`
+	Description       string    `json:"description"`
+	PathWithNamespace string    `json:"path_with_namespace"`
+	CreatedAt         string    `json:"created_at"`
+	WebURL            string    `json:"web_url"`
+	Namespace         Namespace `json:"namespace"`
 }
 
 func main() {
-	var responseBody bytes.Buffer
-	var gp []gitlabProject
+	var gp []GitlabProject
 
 	err := godotenv.Load()
 	if err != nil {
@@ -44,18 +42,34 @@ func main() {
 	baseURL := "https://gitlab.com/api/v4"
 
 	// Create the HTTP request
-	req, err := http.NewRequest(http.MethodGet, baseURL+"/projects?owned=true", &responseBody)
+	req, err := http.NewRequest(http.MethodGet, baseURL+"/projects?owned=true", nil)
+	if err != nil {
+		log.Fatal("Error creating request:")
+	}
+
 	req.Header.Add("PRIVATE-TOKEN", gitlabPAT)
 
-	// Perform the request
-	resp, _ := client.Do(req)
-	body, _ := io.ReadAll(resp.Body)
-	resp.Body.Close()
+	// Send request
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Error performing request:", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("Error reading response body:", err)
+	}
+
+	// Check status code
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("GitLab API error: %s\n%s", resp.Status, string(body))
+	}
 
 	err = json.Unmarshal(body, &gp)
 	if err != nil {
-		log.Fatalln("error at json.Unmarshal: ", err)
+		log.Fatal("Error unmarshalling JSON:", err)
 	}
 
-	fmt.Println(gp[len(gp)-1].created_at)
+	fmt.Println(gp)
 }
