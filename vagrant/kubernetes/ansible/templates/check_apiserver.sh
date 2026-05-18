aa10d2554f9d27d8cfa5833735eb78ab6e39afd1
Nemanja Tomic <nemanja.tomic@ik.me>
2026-05-18 16:54:03 +0200

    update kubernetes

--- vagrant/kubernetes/ansible/templates/check_apiserver.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-errorExit() {
-    echo "*** $*" 1>&2
-    exit 1
-}
-
-curl -sfk --max-time 2 https://localhost:{{ apiserver_dest_port }}/healthz -o /dev/null || errorExit "Error GET https://localhost:{{ apiserver_dest_port }}/healthz"
--- vagrant/kubernetes/provision.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cd ansible
-
-ansible-playbook kubeadm-install.yml -i hosts
-ansible-playbook controlplanes-config.yml -i hosts
