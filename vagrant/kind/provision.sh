b4e920ea1ba7909d1c49a9ff88e14c66a1ad277f
Nemanja Tomic <nemanja.tomic@ik.me>
2026-07-09 16:02:34 +0200

    Finish CGI challenge

--- vagrant/kind/provision.sh
+++ vagrant/kind/provision.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -xeu
 
 vagrant up
-ansible-playbook -i ./inventory.yml ./playbook.yml --timeout 30
+ansible-playbook -i ./inventory.yml ./k8s-cluster-creation.yml --timeout 30
+ansible-playbook -i ./inventory.yml ./k8s-configuration.yml
